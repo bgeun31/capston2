8549da1408f941d36d2ca8a4ea0f74c8148a0766
DESKTOP-8SJBBH2\NLab <sskm0116@naver.com>
2025-04-30 23:33:41 +0900

    Revert "장성진 바보라고 썼음"
    This reverts commit a69022031700f32f6c3a091091ecda683a28c585.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,6 @@ import DashboardPage from "./pages/DashboardPage";
 import TopologyPage from "./pages/TopologyPage";
 import TerminalPage from "./pages/TerminalPage";
 import LogPage from "./pages/LogPage";
-import IDSPage from "./pages/IDSPage";
 
 export default function App() {
   return (
@@ -12,7 +11,6 @@ export default function App() {
       <Route path="/" element={<DashboardPage />} />
       <Route path="/topology" element={<TopologyPage />} />
       <Route path="/terminal" element={<TerminalPage />} />
-      <Route path="/ids" element={<IDSPage />} />
       <Route path="/logs" element={<LogPage />} />
     </Routes>
   );
